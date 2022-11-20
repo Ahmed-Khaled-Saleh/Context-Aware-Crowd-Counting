39307bbfa519fb9400e14dc0212720eb0c6c68a2
Ahmed Khaled <ahmed_khaled3cs@ci.suez.edu.eg>
2022-11-21 01:25:39 +0200

    ss

--- new_train.py
+++ new_train.py
@@ -130,7 +130,7 @@ def get_heatmap(image, boxs, heatmap_size=None):
 
 preprocess = transforms.Compose([
                                 transforms.ToPILImage(),
-                                transforms.Resize(64),
+                                transforms.Resize((64, 64)),
                                 transforms.ToTensor(),
                                 transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
                                 ])
