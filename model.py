fb6441df72fc3136ff965c7806060c61d672c599
Ahmed Khaled <ahmed_khaled3cs@ci.suez.edu.eg>
2022-11-21 01:52:45 +0200

    ss

--- model.py
+++ model.py
@@ -37,7 +37,12 @@ class CANNet(nn.Module):
         self.frontend_feat = [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 'M', 512, 512, 512]
         self.backend_feat  = [512, 512, 512, 256, 128, 64]
         self.frontend = make_layers(self.frontend_feat)
-        self.backend = make_layers(self.backend_feat,in_channels = 512,batch_norm=True, dilation = True)
+        self.backend = make_layers(self.backend_feat,
+                                   in_channels = 512,
+                                   batch_norm=True,
+                                   dilation = True,
+                                   backend = True)
+
         self.output_layer = nn.Conv2d(64, 1, kernel_size=1)
         if not load_weights:
             mod = models.vgg16(pretrained = True)
@@ -62,7 +67,7 @@ class CANNet(nn.Module):
                 nn.init.constant_(m.weight, 1)
                 nn.init.constant_(m.bias, 0)
 
-def make_layers(cfg, in_channels = 3,batch_norm=False,dilation = False):
+def make_layers(cfg, in_channels = 3,batch_norm=False,dilation = False, backend=False):
     if dilation:
         d_rate = 2
     else:
@@ -70,7 +75,10 @@ def make_layers(cfg, in_channels = 3,batch_norm=False,dilation = False):
     layers = []
     for v in cfg:
         if v == 'M':
-            layers += [nn.MaxPool2d(kernel_size=2, stride=1)]
+            if ~backend:
+                layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
+            else:
+                layers += [nn.MaxPool2d(kernel_size=3, stride=2)]
         else:
             conv2d = nn.Conv2d(in_channels, v, kernel_size=3, padding=d_rate, dilation = d_rate)
             if batch_norm:
