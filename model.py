86fe1f8a4a1f97db6d9ffadb8b46b24ccbd05099
Ahmed Khaled <ahmed_khaled3cs@ci.suez.edu.eg>
2022-11-21 01:03:17 +0200

    new

--- model.py
+++ model.py
@@ -42,9 +42,8 @@ class CANNet(nn.Module):
         if not load_weights:
             mod = models.vgg16(pretrained = True)
             self._initialize_weights()
-            for i in range(len(self.frontend.state_dict().items())):
-                print(self.frontend.state_dict().items())
-                self.frontend.state_dict().items()[i][1].data[:] = mod.state_dict().items()[i][1].data[:]
+            for i, key in enumerate(self.frontend.state_dict().keys()):
+                self.frontend.state_dict()[key][1].data[:] = mod.state_dict()[key][1].data[:]
 
     def forward(self,x):
         x = self.frontend(x)
