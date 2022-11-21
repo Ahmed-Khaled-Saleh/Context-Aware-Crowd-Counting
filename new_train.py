a1638161271deab49a2d1f5a49a46feb1a68c90f
Ahmed Khaled <ahmed_khaled3cs@ci.suez.edu.eg>
2022-11-21 02:38:34 +0200

    as

--- new_train.py
+++ new_train.py
@@ -263,21 +263,8 @@ def validate(val_loader, model, criterion):
         count += 1
         if count > 50: break
         img, target = d
-        h,w = img.shape[2:4]
-        h_d = h/2
-        w_d = w/2
-        img_1 = Variable(img[:,:,:h_d,:w_d].cuda())
-        img_2 = Variable(img[:,:,:h_d,w_d:].cuda())
-        img_3 = Variable(img[:,:,h_d:,:w_d].cuda())
-        img_4 = Variable(img[:,:,h_d:,w_d:].cuda())
-        density_1 = model(img_1).data.cpu().numpy()
-        density_2 = model(img_2).data.cpu().numpy()
-        density_3 = model(img_3).data.cpu().numpy()
-        density_4 = model(img_4).data.cpu().numpy()
-
-        pred_sum = density_1.sum()+density_2.sum()+density_3.sum()+density_4.sum()
-
-        mae += abs(pred_sum-target.sum())
+        density = model(img).data.cpu().numpy()
+        mae += abs(density.sum() - target.sum())
 
     mae = mae/len(val_loader)
     print(' * MAE {mae:.3f} '
